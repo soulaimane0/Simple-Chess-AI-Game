93aaa3a48949f1cba4301e3b332d802df0b58b0e
Soulaimane <82826040+soulaimane0@users.noreply.github.com>
2023-06-09 19:13:13 +0100

    Update changes

--- script.js
+++ script.js
@@ -2,22 +2,6 @@ var board,
   game = new Chess();
 
 let positionCount;
-// let minimaxRoot = (depth, game, isMaximisingPlayer) => {
-//   let newGameMoves = game.ugly_moves();
-//   let bestMove = -9999;
-//   let bestMoveFound;
-
-//   newGameMoves.forEach((newGameMove) => {
-//     game.ugly_move(newGameMove);
-//     let value = minimax(depth - 1, game, -10000, 10000, !isMaximisingPlayer);
-//     if (value >= bestMove) {
-//       bestMove = value;
-//       bestMoveFound = newGameMove;
-//     }
-//   });
-//   return bestMoveFound;
-// };
-
 let minimaxRoot = (depth, game, isMaximisingPlayer) => {
   let newGameMoves = game.ugly_moves();
   let bestMove = -9999;
@@ -192,18 +176,56 @@ const onDragStart = (source, piece, position, orientation) => {
   }
 };
 
+const playSound = (flag, isInCheck, isGameOver) => {
+  const self = document.querySelector('#self_move');
+  const capture = document.querySelector('#capture_move');
+  const castle = document.querySelector('#castle_move');
+  const promote = document.querySelector('#promote_move');
+  const check = document.querySelector('#check_move');
+  const gameEnds = document.querySelector('#game_ends');
+
+  if (isGameOver) gameEnds.play();
+  else if (isInCheck && !game.game_over()) check.play();
+  else if (flag === 'n' || flag === 'b' || flag === 1 || flag === 4)
+    self.play();
+  else if (flag === 'c' || flag === 'e' || flag === 2 || flag === 8)
+    capture.play();
+  else if (flag === 'k' || flag === 'q' || flag === 32 || flag === 64)
+    castle.play();
+  else if (flag === 'p' || flag === 16) promote.play();
+};
+
+const removeInCheckClass = () => {
+  $('.square-55d63').removeClass('in_check');
+};
+
 const makeBestMove = () => {
   let bestMove = getBestMove(game);
   game.ugly_move(bestMove);
   board.position(game.fen());
+  playSound(bestMove?.flags, game.in_check(), game.game_over());
+
+  const wKingSquare = $('.square-55d63').has('img[data-piece="wK"]');
+  if (game.in_check() && wKingSquare.length > 0) {
+    wKingSquare.addClass('in_check');
+  } else {
+    removeInCheckClass();
+  }
+
   renderMoveHistory(game.history());
   if (game.game_over()) {
-    alert('Game over');
+    setTimeout(() => {
+      alert('Game Over !');
+    }, 250);
   }
 };
 
 const getBestMove = (game) => {
-  if (game.game_over()) alert('Game over');
+  if (game.game_over()) {
+    setTimeout(() => {
+      alert('Game Over !');
+    }, 250);
+  }
   positionCount = 0;
   let depth = parseInt($('#search-depth').find(':selected').text());
 
@@ -242,12 +264,19 @@ const onDrop = (source, target) => {
   });
 
   removeGreySquares();
-  if (move === null) return 'snapback';
+  playSound(move?.flags, game.in_check(), game.game_over());
 
+  const bKingSquare = $('.square-55d63').has('img[data-piece="bK"]');
+  if (game.in_check() && bKingSquare.length > 0) {
+    bKingSquare.addClass('in_check');
+  } else {
+    removeInCheckClass();
+  }
+
+  if (move === null) return 'snapback';
   renderMoveHistory(game.history());
   window.setTimeout(makeBestMove, 250);
 };
-
 const removeGreySquares = () => {
   $('#board .square-55d63').css('background', '');
 };
@@ -295,18 +324,21 @@ const opPosition = (gameFen, isBlacksMove) => {
 };
 
 $('#ruyLopez').on('click', () => {
+  removeInCheckClass();
   const fen =
     'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 0 1';
   opPosition(fen, true);
 });
 
 $('#italianGame').on('click', () => {
+  removeInCheckClass();
   const fen =
     'r1bqkbnr/pppp1ppp/2n5/4p3/2B1P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 0 1';
   opPosition(fen, true);
 });
 
 $('#sicillianDefense').on('click', () => {
+  removeInCheckClass();
   const fen = 'rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1';
   opPosition(fen, false);
 });
@@ -314,28 +346,20 @@ $('#sicillianDefense').on('click', () => {
 let defaultPosition =
   'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
 $('#startPosition').on('click', () => {
+  removeInCheckClass();
   opPosition(defaultPosition, false);
+  document.querySelector('#game_start').play();
 });
 
 $('#reset').on('click', () => {
-  if (confirm('Are you sure you want to restart?')) {
+  if (
+    game.fen() === 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+  ) {
+    return;
+  } else if (confirm('Are you sure you want to restart?')) {
+    removeInCheckClass();
     opPosition(defaultPosition, false);
-  }
-});
-
-$('#showHint').on('click', () => {
-  let bestMove = getBestMove(game);
-  console.log(bestMove);
-});
-
-const undo = () => {
-  game.undo();
-  board.position(game.fen());
-};
-
-$('#previous').on('click', () => {
-  for (let i = 0; i < 2; i++) {
-    undo();
+    document.querySelector('#game_start').play();
   }
 });
 
